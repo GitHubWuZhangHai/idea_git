6569e0d6255bfc4c42614717812cdb8298cf270f
wuzhanghai <wu_zhang_hai@163.com>
2019-06-30 19:12:55 +0800

    提交User

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,12 @@
+package pojo;
+
+/**
+ * @author wu_zh
+ * @Date 2019/6/30 19:04
+ * @Classname User
+ * @Description TODO
+ */
+public class User {
+    private String name;
+    private int age;
+}
